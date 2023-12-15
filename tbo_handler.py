8e0cdee5ce1179d515d46d013b5f2759e308a54a
Aviansh1234 <74455180+Aviansh1234@users.noreply.github.com>
2023-12-15 17:22:38 +0530

    Updated TBO handler

--- tbo_handler.py
+++ tbo_handler.py
@@ -21,3 +21,11 @@ def get_hotel_info(hotel_id):
     else:
         return None
     pass
+def get_city_code(country_code, city_name):
+    response = requests.post(config.tbo_base_url + "/CityList", auth=creds, json={'CountryCode': country_code})
+    if response.status_code == 200:
+        for i in response.json()['CityList']:
+            if city_name in i['Name']:
+                return i["Code"]
+        return 'City Not Found'
+    return '-1'
