ac050847cd21b28529951e4d835ba813450f42d4
Aviansh1234 <74455180+Aviansh1234@users.noreply.github.com>
2023-12-17 15:02:02 +0530

    Added get hotel info endpoint

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
+import tbo_handler as tbo
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/getHotelInfo")
+async def get_hotel_info(hotelId):
+    return tbo.get_hotel_info(hotelId)
