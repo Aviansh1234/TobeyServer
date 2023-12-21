02f7e76c3e1a8693ae866509734b26ca09c125f0
Tejash Prasad <tejashprasad@gmail.com>
2023-12-21 19:49:37 +0530

    get_user_data and shortlist_hotels implemented in bard handler.

--- bard_handler.py
+++ bard_handler.py
@@ -33,7 +33,7 @@ def get_full_user_details(messages):
         }
     }
     Your top priority is to fill the json file with the data you'll ask from the user and output it as soon as you're done. You'll only ask one question as a time, like in standard conversations with a travel agent and make the user feel at home. You'll not ask for data in any specific format, but will take whatever data the user gives you and parse it accordingly, i.e. you'l never say the word format.
-    You will respond only with questions and nothing else. You will not ask anything which is not required to fill in the json and ask only relevant questions required to fill the json file first and when you're done, you'll output the json. After that, you'll keep asking for more preferences from the user.
+    You will respond only with questions and nothing else. You will not ask anything which is not required to fill in the json and ask only relevant questions required to fill the json file first and when you're done, you'll output the json. After that, you'll keep asking for more preferences from the user. First, you'll greet the user by introducing yourself as the AI assistant for TBO.com and ask for the destination city in the same message.
     You will follow the order of questions : 
     1. Their destination city. You'll get the country name based on the destination city. In case there are two cities of the same name in different countries, you'll ask the user to clarify, but only when necessary.
     2. Their check-in and check-out dates
@@ -48,11 +48,11 @@ def get_full_user_details(messages):
     You will leave no field null, unless the user declines to answer the question on that. Hence, you'll ensure that all the details have been asked for from your side.
 
     After you've asked all of these questions, you'll confirm the details one last time as a summary and then, once the user confirms, you'll say "Received hihihiha" and output the json file containing all the information collected from the user, filled in the same format as the given sample json in the same message, and not write anything else in the message. You will output the json in the end as per my requirement without me asking for it andd it will always be preceeded by "Received hihihiha".
-     Ask the user for the data in a very friendly and engaging way while also passing some playful comments. Don't forget to output the json in the end. As soon as the user confirms, you'll respond with the json.
+     Ask the user for the data in a very friendly and engaging way while also passing some playful comments. Don't forget to output the json in the end. As soon as you're done with collecting all the data mentioned in the json and the user confirms, you'll respond with the json immediately.
     """
 
     chatHistory.append({"role": "user", "parts": [initPrompt]})
-    chatHistory.append(messages)
+    chatHistory.extend(messages)
     response = model.generate_content(chatHistory,
                                       generation_config={"temperature": 0.9, "top_p": 1,
                                           "stop_sequences": ["X"]},
@@ -67,34 +67,35 @@ def get_full_user_details(messages):
                                           }
                                       ]
                                       )
+
     return response.text
 
 
 def short_list_hotels(messages, hotels):
-    initPrompt = f""" Now, based on the conversation above, suggest which hotels I'd like from the given list : {str(hotels)}. If you need more information, other than the info already provided previously, to short-list them for the user, ask for more information, but one by one, and give a short-listed list of hotels in the end. """
+    initPrompt = f""" Now, based on the conversation above, suggest which hotels the user would like from the given list : {str(hotels)}.You'll not ask for any more information, you'll only output a python string list containign the names of the hotels which you think will be preferred by the user,based on the previous conversation, among the list of hotels given to you. """
     messages.append({"role": "user", "parts": [initPrompt]})
 
-    while True:
-        response = model.generate_content(messages,generation_config={"temperature": 0.9, "top_p": 1,
-                                                  "stop_sequences": ["X"]},
-                                              safety_settings=[
-                                                  {
-                                                      "category": "HARM_CATEGORY_HARASSMENT",
-                                                      "threshold": "BLOCK_NONE",
-                                                  },
-                                                  {
-                                                      "category": "HARM_CATEGORY_HATE_SPEECH",
-                                                      "threshold": "BLOCK_NONE"
-                                                  }
-                                              ])
-        print(f"respone feedback : {response.prompt_feedback}")
-        print(f"Gemini: {response.text}")
-        messages.append({"role": "model", "parts": [response.text]})
-        message = input("You : ")
-        if (len(message.strip()) < 1):
-            print("You haven't entered anything!")
-            continue
-        messages.append({"role": "user", "parts": [message]})
+    response = model.generate_content(messages,generation_config={"temperature": 0.9, "top_p": 1,
+                                              "stop_sequences": ["X"]},
+                                          safety_settings=[
+                                              {
+                                                  "category": "HARM_CATEGORY_HARASSMENT",
+                                                  "threshold": "BLOCK_NONE",
+                                              },
+                                              {
+                                                  "category": "HARM_CATEGORY_HATE_SPEECH",
+                                                  "threshold": "BLOCK_NONE"
+                                              }
+                                          ])
+    return response.text
+    # print(f"respone feedback : {response.prompt_feedback}")
+    # print(f"Gemini: {response.text}")
+    # messages.append({"role": "model", "parts": [response.text]})
+    # message = input("You : ")
+    # if (len(message.strip()) < 1):
+    #     print("You haven't entered anything!")
+    #     continue
+    # messages.append({"role": "user", "parts": [message]})
 
 def show_hotels_creatively(hotels):
     pass
@@ -103,8 +104,17 @@ def show_hotels_creatively(hotels):
 
 def main():
     messages = []
-    hotels = ["Alila Diwa, Goa", "Lemon Tree Hotel, Candolim, Goa", "Grand Hyat, Goa", "Moustache Goa Luxuria"]
-    dictString = get_full_user_details(messages)
+    hotels = ["Alila Diwa, Goa", "Lemon Tree Hotel, Candolim, Goa", "Grand Hyatt, Goa", "Moustache Goa Luxuria"]
+    while True:
+        dictString = get_full_user_details(messages)
+        messages.append({"role": "model", "parts": [dictString]})
+        print(dictString)
+        message = input("You: ")
+        messages.append({"role": "user", "parts": [message]})
+        if("Received hihihiha" in dictString):
+            print("breaking")
+            break
+
     if (dictString != "") :
         print(dictString)
         dictString = dictString.replace("Received hihihiha", "")
@@ -112,6 +122,6 @@ def main():
         print(dictString)
         userData = eval(dictString)
         print(userData)
-    short_list_hotels(messages, hotels)
+        short_list_hotels(messages, hotels)
 
 main()
