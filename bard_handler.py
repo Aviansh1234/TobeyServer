f6573f3a019dabde5edd8ac40639f0e3cf942b97
Tejash Prasad <tejashprasad@gmail.com>
2023-12-28 16:47:22 +0530

    show_hotels_creatively made functional.

--- bard_handler.py
+++ bard_handler.py
@@ -37,14 +37,14 @@ def get_full_user_details(messages):
     You will respond only with questions and nothing else. You will not ask anything which is not required to fill in the json and ask only relevant questions required to fill the json file first and when you're done, you'll output the json. After that, you'll ask once for more preferences from the user. First, you'll greet the user by introducing yourself as the AI assistant for TBO.com and ask for the destination city in the same message.You'llbe extremely friendly and will make the conversation very engaging while not pushing the user for any info or being repetitive. You'll see your response for any dump values first and not try to fill in any random words.
     You will follow the order of questions : 
     1. Their destination city. You'll get the country name based on the destination city, and will fill in the country code, example IN for India, US for United States, etc. In case there are two cities of the same name in different countries, you'll ask the user to clarify, but only when necessary. Do not forget to fill the CountryName field under any circumstances.
-    2. Their check-in and check-out dates
-    3. Number of travellers and required number of rooms
-    4. Composition of travellers(as children and adults)
-    5. ages of children (if any). You'll ask for their ages and check if they're below 18. If not, you'll prompt the user and ask for the details again. You'll fill in these ages in the corresponding list in paxrooms field.
+    2. Their check-in and check-out dates, this can be given in any format, even natural language.
+    3. Number of travellers and required number of rooms, this is necessary, don't forget to ask for this.
+    4. Composition of travellers(as children and adults). This also is important, so don't forget to ask for this.
+    5. ages of children (if any). You'll ask for their ages and check if they're below 18. If not, you'll prompt the user and ask for the details again. You'll fill in these ages in the corresponding list in paxrooms field. This information is crucial and so you'll not forget filling this field, correctly for each room. Hence, you'll not forget to ask for the ages of children as well, and check clearly which child is in what room.
     6. composition of the rooms (how many adults and how many children in a specific room). You will not forget to ask for this, this is essential.
-    7. their nationality and preferred currency of payment
-    8. The preferred meal plan with their hotel booking.
-    9. Their preference on refundability.
+    7. their nationality and preferred currency of payment. The nationality must also be written in the form of country code, for exaxmple, IN for India, US for United States, UAE for Dubai and so on.
+    8. The preferred meal plan with their hotel booking. This information is also necessary, don't forget to ask for this.
+    9. Their preference on refundability. This information is also necessary, don't forget to ask for this.
     You'll ask all the questions in a very friendly, playful manner and will not keep pushing for the answer, and will not the details of the json to the user. You'll act calm and confident and not bug the user for answers. There should not be more than 3 sentences in one message.
     In case the user forgets to answer a question you've asked, ask the question again. If the user isn't sure of the answer, don't put any pressure and populate that field with -1 in the json.
     You will not output the json unless you've confirmed the validity of all the data from the user.
@@ -99,33 +99,49 @@ def short_list_hotels(messages, hotels):
 
 def show_hotels_creatively(messages, hotels):
     chatHistory = []
-    initPrompt =f""" Now, based on the conversation above, this is a list of hotels which have been shortlisted : {str(hotels)}. Now, I need you to give me pros and cons of each one and present them all very creatively. Here's the format you must follow : {"hotelName" : {"description" : "a creative description of the hotel, relevant to how the user requested it", "pros" : "how this hotel suits the user's needs, determined based on the previous conversation", "cons": "how this has certain shortcomings on the user's request, again based on the previous conversation"}}"""
-    chatHistory.extend(messages)
-    chatHistory.append({"role": "user", "parts": [initPrompt]})
-    response = model.generate_content(chatHistory,
-                                      generation_config=genai.types.GenerationConfig(stop_sequences=['X'], top_p=0.9,
-                                                                                     temperature=0.9),
-                                      safety_settings=[
-                                          {
-                                              "category": "HARM_CATEGORY_HARASSMENT",
-                                              "threshold": "BLOCK_NONE",
-                                          },
-                                          {
-                                              "category": "HARM_CATEGORY_HATE_SPEECH",
-                                              "threshold": "BLOCK_NONE"
-                                          }
-                                      ]
-                                      )
+    reviews = []
+    for hotel in hotels:
 
+        initPrompt =f""" Now, based on the conversation above, this is a hotel which has been shortlisted : {str(hotel)}. Now, I need you to give me pros and cons of this hotel and present it very creatively, in natural language, not in any format, while carefully assessing the user's needs and what features of the hotel align with them, and what don't.Your review will be personalised for the user, and should address the needs of the user, and will write the review in a tone of talking to the user as a friend.  You'll ensure that all reviews are under 100 words, and are fun to read."""
+        chatHistory.extend(messages)
+        chatHistory.append({"role": "user", "parts": [initPrompt]})
+        response = model.generate_content(chatHistory,
+                                          generation_config=genai.types.GenerationConfig(stop_sequences=['X'], top_p=0.9,
+                                                                                         temperature=0.9),
+                                          safety_settings=[
+                                              {
+                                                  "category": "HARM_CATEGORY_HARASSMENT",
+                                                  "threshold": "BLOCK_NONE",
+                                              },
+                                              {
+                                                  "category": "HARM_CATEGORY_HATE_SPEECH",
+                                                  "threshold": "BLOCK_NONE"
+                                              }
+                                          ]
 
-    return response.text
+                                          )
+        chatHistory.remove({"role": "user", "parts": [initPrompt]})
+        # print(response.text)
+        reviews.append(response.text)
+    return reviews
 
 
 # def main():
-#     messages = [{'role': 'model', 'parts': ["Hi there, I'm Tobey, your AI assistant from TBO.com. Where would you like to travel to?"]}, {'role': 'user', 'parts': ['Goa seems nice']}, {'role': 'model', 'parts': ["Sure, Goa it is. When are you planning on checking in? Tell me in any format you like, I'll understand. :) "]}, {'role': 'user', 'parts': ['how about 30th of december /']}, {'role': 'model', 'parts': ["Noted. And when would you like to check out? Tell me in your own words. I'll catch on. :D "]}, {'role': 'user', 'parts': ["we'll be there for 5 nights"]}, {'role': 'model', 'parts': ["Okay, that means you'll be checking out on the 4th of January, correct? Just want to be doubly sure. :) "]}, {'role': 'user', 'parts': ['yes']}, {'role': 'model', 'parts': ['Great! Now, how many of you will be travelling? Feel free to tell me the number of adults and children separately. :D ']}, {'role': 'user', 'parts': ['there are 3 adults and 2 children']}, {'role': 'model', 'parts': ["Sounds like a family trip! How many rooms will you need? Let me know, and we'll move forward. :D "]}, {'role': 'user', 'parts': ["we'll need 2 rooms"]}, {'role': 'model', 'parts': ['Alright! And how will the rooms be occupied? Let me know the split-up of adults and children in each room. :D \n(If there are any children below the age of 18, please mention their ages as well.)']}, {'role': 'user', 'parts': ['2 adults and a child in one room, 1 adult and a child in the other']}, {'role': 'model', 'parts': ['Got it! Just to confirm, in the first room, there will be 2 adults and a child, and in the second room, there will be 1 adult and 1 child, correct? ']}, {'role': 'user', 'parts': ['yes']}, {'role': 'model', 'parts': ['Wonderful! Now, could you please tell me the nationality of all the travelers and the currency you prefer for the payment? :D ']}, {'role': 'user', 'parts': ["we're all indians"]}, {'role': 'model', 'parts': ['Noted! And which currency would you like to use for the payment? Just the currency code is fine. :)']}, {'role': 'user', 'parts': ['inr']}, {'role': 'model', 'parts': ["Alright! What kind of meal plan would you prefer with your hotel booking? It can be 'All', 'WithMeal', or 'RoomOnly'. Let me know your preference. :D"]}, {'role': 'user', 'parts': ['breakfast included']}, {'role': 'model', 'parts': ["Sure! And would you like your hotel booking to be refundable or non-refundable? :)']}, {'role': 'user', 'parts': ['refundable']}, {'role': 'model', 'parts': [Excellent! Just to confirm, you'd like to travel to Goa, check in on the 30th of December and check out on the 4th of January. There will be 3 adults and 2 children traveling in 2 rooms, with 2 adults and a child in one room and 1 adult and a child in the other. You're all Indian nationals and would like to pay in INR. You'd like a refundable booking with breakfast included. Is this information correct?"]}, {'role': 'user', 'parts': ['yes']}, {'role': 'model', 'parts': ["""Received hihihiha {"CheckIn": "2023-12-30", "CheckOut": "2024-01-04", "City": "Goa", "CountryName": null, "GuestNationality": "IN", "PreferredCurrencyCode": "INR", "PaxRooms": [{"Adults": 2, "Children": 1, "ChildrenAges": []}, {"Adults": 1, "Children": 1, "ChildrenAges": []}], "Filters": {"MealType": "WithMeal", "Refundable": "true"}}
-# """]}]
-#
-#     hotels = ["Grand Hyatt, Goa","Alila Diwa, Goa",  "Holiday Inn Resort, Goa", "Radisson Goa", "Caravela Beach Resort", "Goa Mariott Resort and Spa"]
-#     print(short_list_hotels(messages, hotels))
-#
+#     messages = []
+#     hotels = ["Alila Diwa, Goa", "Grand Hyatt, Goa", "Caravela Beach Resort, Goa", "The Lalit Golf and Spa Resort, Goa", "Taj Holiday Village Resort and Spa", "Holiday Inn resort, Goa"]
+#     while True:
+#         response = get_full_user_details(messages)
+#         messages.append({"role": "model", "parts": [response]})
+#         if "PaxRooms" in response:
+#             break
+#         print(response)
+#         query = input("You: ")
+#         if query == "":
+#             print("Not a valid entry. Try again.")
+#             continue
+#         messages.append({"role": "user", "parts": [query]})
+#     short_listed_hotels = short_list_hotels(messages, hotels)
+#     print(short_listed_hotels)
+#     reviews = show_hotels_creatively(messages, short_listed_hotels)
+#     print(reviews)
 # main()
